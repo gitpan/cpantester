012ed7b80a2255e395d6a324d8ff6a655c061044
Steven Schubiger (stsc) <schubiger@cpan.org>
2005-02-25 07:09:25 +0000

    Import of SCHUBIGER/cpantester-0.01_01 from CPAN.
    gitpan-cpan-distribution: cpantester
    gitpan-cpan-version:      0.01_01
    gitpan-cpan-path:         SCHUBIGER/scripts/cpantester-0.01_01.tar.gz
    gitpan-cpan-author:       SCHUBIGER
    gitpan-cpan-maturity:     developer

--- cpantester.pl
+++ cpantester.pl
@@ -7,42 +7,237 @@ use vars qw(
     $VERBOSE
     $INTERACTIVE
     $PROMPT
-    %conf
     $ftp
 );
 use warnings;
+use File::Slurp;
 use Getopt::Long;
 use Net::FTP;
 use Test::Reporter;
 
 
-$VERSION = '0.01_00';
-$NAME = 'cpantester';
 
-
-$PROMPT = "\n#";
+$PROMPT      = "\n#";
 $INTERACTIVE = 0;
-$VERBOSE  = 0;
+$VERBOSE     = 0;
 
 
-parse_args();
-test( fetch(), read_track() );
 
+{
+    my $conf = parse_args();
+    test( $conf, fetch( $conf ), read_track( $conf ) );
+}
+
+sub test {
+    my ($conf, $files, $got_file) = @_;
+    
+    my $TRACK = \*TRACK;
+    open ($TRACK, ">>$conf->{track}") or err_quit( $conf->{track}, 'o+' );
+
+    for my $file (@{$files}) {
+        next if ($got_file->{$file} || $file !~ /tar.gz$/);
+      
+        $ftp->get( $file, "$conf->{dir}/$file" )
+          or die "Couldn't get $file: ", $ftp->message;
+	  
+	my $file = "$conf->{dir}/$file";
+	
+	my $error = 0;
+	
+	chdir ( "$conf->{dir}" ) or die "Couldn't cd to $conf->{dir}: $!\n";
+	
+	if ($INTERACTIVE) { 
+	    print "$PROMPT tar xvvzf $file -C $conf->{dir}? ";
+	    <STDIN>; 
+	} elsif ($VERBOSE) { 
+	    warn "tar xvvzf $file -C $conf->{dir}...\n" 
+	}
+	
+    	my @tar = `tar xvvzf $file -C $conf->{dir}`;
+	if ($VERBOSE) { warn @tar }
+	die "Could not tar xvvzf $file -C $conf->{dir}: $?\n" if ($? != 0);
+	
+    	my ($dist) = $file =~ /(.*)\.tar.gz$/;
+	my $dist_dir = "$conf->{dir}/$dist";
+	
+    	unless (chdir ( "$dist_dir" )) {
+	    warn "Could not cd to $dist_dir, skipping...\n"; 
+	    $error = 1; 
+	}
+	
+	$error = 1 if (-e "$dist_dir/Build.PL" || ! -e "$dist_dir/Makefile.PL");
+	
+	unless ($error) {
+   	    if ($INTERACTIVE) { 
+	        print "$PROMPT perl Makefile.PL? ";
+	        <STDIN>; 
+	    }
+	    my @makefile = `perl Makefile.PL`;
+	    die "perl Makefile.PL exited on $?\n" if ($? != 0);
+	    if ($VERBOSE) {
+	        warn "perl Makefile.PL...\n" unless $INTERACTIVE;
+	        warn @makefile;
+	    }
+	
+	    for my $line (@makefile) {
+	        if ($line =~ /(?:error|not found)/i) {
+	            print "Prerequisites missing, skipping...\n";
+		    $error = 1; last TEST;
+	        }
+	    }
+        }
+        TEST:
+	
+        unless ($error) {
+            if ($INTERACTIVE) { 
+	        print "$PROMPT make? ";
+	        <STDIN>; 
+	    }
+	    my @make = `make`;
+	    die "make exited on $?\n" if ($? != 0);
+	    if ($VERBOSE) {
+	        warn "make...\n" unless $INTERACTIVE;
+	        warn @make;
+	    }
+	    
+	    if ($INTERACTIVE) { 
+	        print "$PROMPT make test? ";
+	       <STDIN>; 
+	    }
+	    my @maketest = `make test`;
+	    die "make test exited on $?\n" if $? != 0;
+	    if ($VERBOSE) {
+	        warn "make test...\n" unless $INTERACTIVE;
+		warn @maketest;
+	    }
+	    {
+	        my $reporter = Test::Reporter->new();
+		
+	        $reporter->debug( $VERBOSE ) if $VERBOSE;
+	    
+	        $reporter->from( $conf->{mail} );
+	        $reporter->distribution( $dist );
+		    
+	        my $failed = 0;
+	    
+	        for my $line (@maketest) {
+	            next unless $line =~ /failed/i;
+		    $failed = 1;
+		    last;
+	        }
+		 
+                if ($failed) { 
+		    $reporter->grade('fail');
+		} else { 
+		    $reporter->grade('pass');
+		}
+		 
+                $reporter->send() or die $reporter->errstr();
+            }
+    	    
+            if ($INTERACTIVE) { 
+                print "$PROMPT make realclean? ";
+                <STDIN>;
+            }
+            my @makerealclean = `make realclean`;
+            die "make realclean exited on $?\n" if ($? != 0);
+            if ($VERBOSE) {
+	        warn "make realclean...\n" unless $INTERACTIVE;
+                warn @makerealclean;
+            }
+    	    
+            if ($INTERACTIVE) {
+                print "$PROMPT rm -rf $dist_dir? ";
+                <STDIN>;
+            }
+            my @rm = `rm -rf $dist_dir`;
+            die "rm -rf exited on $?\n" if ($? != 0);
+            if ($VERBOSE) {
+	        warn "rm -rf $dist_dir...\n" unless $INTERACTIVE;
+                warn @rm;
+	    }
+        }
+	
+        print $TRACK "$file\n";
+    }
+
+    close ($TRACK) or err_quit( $conf->{track}, 'c' );
+
+    $ftp->quit;
+}
+
+sub fetch {
+    my ($conf) = @_;
+    
+    my @err_msg = (
+        "Couldn't connect to $conf->{host}:",
+	"Couldn't login: ",  
+        "Couldn't switch to binary mode: ",
+	"Couldn't change working directory: ",
+	"Couldn't get list from $conf->{rdir}: ",
+    );
+
+    $ftp = Net::FTP->new( $conf->{host}, Debug => $VERBOSE )
+      or die "$err_msg[0]: $@\n";
+    
+    $ftp->login( 'anonymous','-anonymous@' )
+      or err_quit_ftp( $err_msg[1], $ftp );
+  
+    $ftp->binary or err_quit_ftp( $err_msg[2], $ftp );
+
+    $ftp->cwd( $conf->{rdir} )
+      or err_quit_ftp( $err_msg[3], $ftp );
+  
+    my @files = $ftp->ls()
+      or err_quit_ftp( $err_msg[4], $ftp );
+
+    @files = sort @files[ 0 .. $#files ];
+    
+    return \@files;
+}
+
+sub read_track {
+    my ($conf) = @_;
+    
+    my (%got_file, $TRACK);
+    $TRACK = \*TRACK;
+    
+    open ($TRACK, $conf->{track}) or err_quit( $conf->{track}, 'o' );
+    chomp (my @files = <$TRACK>);
+    @got_file{ @files } = (1) x @files;
+    close ($TRACK) or err_quit( $conf->{track}, 'o' );
+    
+    return \%got_file;
+}
 
 sub parse_args {
+    my $conf = usage();
+    
+    my $login   = getlogin;
+    my $homedir = $login =~ /(root)/ ? "/$1" : "/home/$login";
+    
+    my $conf_text = read_file( "$homedir/.cpantesterrc" );
+    %{$conf} = $conf_text =~ /^([^=]+?)\s+=\s+(.+)$/gm;
+    
+    return $conf;
+}
+
+sub usage {
+    my @err = @_;
+    
     $Getopt::Long::autoabbrev = 0;
     $Getopt::Long::ignorecase = 0; 
     
-    my %opt;
+    my (%conf, %opt);
     
-    GetOptions(\%opt, 'd', 'h', 'i', 'V') or $opt{'h'} = 1;
+    GetOptions(\%opt, 'h', 'i', 'v', 'V') or $opt{'h'} = 1;
 
     if ($opt{'h'} || $opt{'V'}) {
         if ($opt{'h'}) {
             print <<"";
 usage: $0 [options]
-  -i		interactive (defies -v) 
   -h            this help screen
+  -i		interactive (defies -v) 
   -v		verbose
   -V            version info
 
@@ -56,182 +251,35 @@ usage: $0 [options]
     }
 
     $INTERACTIVE = 1 if $opt{i};
-    $VERBOSE     = 1 if $opt{d};
+    $VERBOSE     = 1 if $opt{v};
     
-    my $login   = getlogin;
-    my $homedir = $login eq 'root' ? '/root' : "/home/$login";
+    return \%conf;
+}   
+
+sub err_quit {
+    my ($err, $mode) = @_;
     
-    my $rc = "$homedir/.cpantesterrc" unless $opt{f};
-    open (RC, $rc) or die "Could not open $rc for reading: $!\n";
-    my @rcconf = <RC>;
-    close (RC) or die "Could not close $rc: $!\n";
+    my %msg = (
+        c    => 'Could not close ',
+	o    => 'Could not open ',
+	'o+' => [ 'Could not open ', ' for reading:', ],
+    );
     
-    for my $line (@rcconf) {
-        chomp $line;
-        my @entry = split /\s+=\s+/, $line; 
-	$conf{$entry[0]} = $entry[1];
+    if (ref $msg{$mode} eq 'ARRAY') {
+        die "${$msg{$mode}}[0]$err${$msg{$mode}}[1] $!\n";
+    } else {
+        die "$msg{$mode}$err: $!\n";
     }
-}    
+} 
 
-sub fetch {
-    my @files;
-
-    $ftp = Net::FTP->new( $conf{host}, Debug => $VERBOSE )
-      or die "Couldn't connect to $conf{host}: $@";
-
-    $ftp->login( "anonymous",'-anonymous@' )
-      or die "Couldn't login: ", $ftp->message;
-  
-    $ftp->binary or die "Couldn't switch to binary mode: ", $ftp->message;
-
-    $ftp->cwd( $conf{rdir} )
-      or die "Couldn't change working $conf{dir}ectory: ", $ftp->message;
-  
-    @files = $ftp->ls()
-      or die "Couldn't get list from $conf{rdir}: ", $ftp->message;
-
-    splice (@files, 0, 2);
-    @files = sort @files;
-    
-    return \@files;
+sub err_quit_ftp { 
+    my $ftp = splice (@_, 1, 1);
+    die "@_", $ftp->message;
 }
 
-sub read_track {
-    my %got_file;
-
-    open (TRACK, $conf{track}) or die "Couldn't open $conf{track} for reading: $!\n";
-    while (my $file = <TRACK>) {
-        chomp $file;
-        $got_file{$file} = 1;
-    }
-    close (TRACK) or die "Couldn't close $conf{track}: $!\n";
-    
-    return \%got_file;
-}
-
-sub test {
-    my ($files, $got_file) = @_;
-    
-    open (TRACK, ">>$conf{track}") or die "Couldn't open $conf{track} for writing: $!\n";
-
-    for (my $i = 0; $i < @{$files}; ) {
-        if ($files->[$i] !~ /tar.gz$/ || $got_file->{$files->[$i]}) {
-            splice (@{$files}, $i, 1);
-        } 
-        else {
-            $ftp->get( $files->[$i], "$conf{dir}/$files->[$i]" )
-              or die "Couldn't get $files->[$i]: ", $ftp->message;
-	
-	    my $error = 0;
-	
-	    if ($INTERACTIVE) { 
-	        print "$PROMPT tar xvvzf $conf{dir}/$files->[$i] -C $conf{dir}? ";
-	        (undef) = <STDIN>; 
-	    }
-	    if ($VERBOSE && !$INTERACTIVE) { warn "tar xvvzf $conf{dir}/$files->[$i] -C $conf{dir}...\n" }
-	
-	    chdir ( "$conf{dir}" ) or die "Couldn't cd to $conf{dir}: $!\n";
-	
-    	    system( "tar xvvzf $files->[$i] -C $conf{dir}" );
-	    die "Could not tar xvvzf $conf{dir}/$files->[$i] -C $conf{dir}: $?\n" if ($? != 0);
-	
-    	    my ($dist) = $files->[$i] =~ /(.*)\.tar.gz$/;
-    	    unless (chdir ( "$conf{dir}/$dist" )) {
-	        warn "Could not cd to $conf{dir}/$dist, skipping...\n"; 
-	        $error = 1; 
-	    }
-	
-	    $error = 1 if (-e "$conf{dir}/$dist/Build.PL" || ! -e "$conf{dir}/$dist/Makefile.PL");
-	
-	    unless ($error) {
-   	        if ($INTERACTIVE) { 
-	            print "$PROMPT perl Makefile.PL? ";
-	            (undef) = <STDIN>; 
-	        }
-		my @makefile = `perl Makefile.PL`;
-		die "perl Makefile.PL exited on $?\n" if ($? != 0);
-	        if ($VERBOSE && !$INTERACTIVE) { warn "perl Makefile.PL...\n" }
-		if ($VERBOSE) { warn @makefile }
-	
-	        for my $line (@makefile) {
-	            if ($line =~ /error/i || $line =~ /not found/) {
-	                $error = 1;
-	                print "Prerequisites missing, skipping...\n";
-		        last TEST;
-	            }
-	        }
-	    }
-	    TEST:
-	
-	    unless ($error) {
-    	        if ($INTERACTIVE) { 
-	            print "$PROMPT make? ";
-		    (undef) = <STDIN>; 
-	        }
-		my @make = `make`;
-		die "make exited on $?\n" if ($? != 0);
-	        if ($VERBOSE && !$INTERACTIVE) { warn "make...\n" }
-		if ($VERBOSE) { warn @make }
-	    
-	        if ($INTERACTIVE) { 
-	            print "$PROMPT make test? ";
-		    (undef) = <STDIN>; 
-	        }
-		my @maketest = `make test`;
-		die "make test exited on $?\n" if $? != 0;
-	        if ($VERBOSE && !$INTERACTIVE) { warn "make test...\n" }
-		if ($VERBOSE) { warn @maketest }
-	        {
-	            my $reporter = Test::Reporter->new();
-		
-		    $reporter->debug( $VERBOSE ) if $VERBOSE;
-	    
-	            $reporter->from( $conf{mail} );
-	            $reporter->distribution( $dist );
-		    
-		    my $failed = 0;
-	    
-	            for my $line (@maketest) {
-	                if ($line =~ /failed/i) {
-		            $failed = 1;
-		            last;
-	               }
-		    }
-		 
-		    $failed 
-		      ? $reporter->grade('fail') 
-		      : $reporter->grade('pass');
-		 
-	            $reporter->send() or die $reporter->errstr();
-	        }
-    	    
-	        if ($INTERACTIVE) { 
-	            print "$PROMPT make realclean? ";
-		    (undef) = <STDIN>;
-	        }
-		my @makerealclean = `make realclean`;
-		die "make realclean exited on $?\n" if ($? != 0);
-	        if ($VERBOSE && !$INTERACTIVE) { warn "make realclean...\n" }
-		if ($VERBOSE) { warn @makerealclean }
-    	    
-	        if ($INTERACTIVE) {
-	            print "$PROMPT rm -rf $conf{dir}/$dist? ";
-	            (undef) = <STDIN>;
-                }
-		my @rm = `rm -rf $conf{dir}/$dist`;
-		die "rm -rf exited on $?\n" if ($? != 0);
-	        if ($VERBOSE && !$INTERACTIVE) { warn "rm -rf $conf{dir}/$dist...\n" }
-		if ($VERBOSE) { warn @rm }
-            }
-	
-	    print TRACK "$files->[$i]\n";
-	    $i++;
-        }
-    }
-    
-    close (TRACK) or die "Couldn't close $conf{track}: $!\n";
-
-    $ftp->quit;
+BEGIN {
+    $VERSION = '0.01_02';
+    $NAME = 'cpantester';
 }
 
 1;
@@ -246,9 +294,8 @@ cpantester - test CPAN contributions and submit reports to cpan-testers@perl.org
  usage: /scripts/cpantester.pl [options]
 
 =head1 OPTIONS
-
-   -i		 interactive (defies -v) 
    -h            this help screen
+   -i		 interactive (defies -v) 
    -v		 verbose
    -V            version info
 
@@ -285,6 +332,6 @@ it's my intention to make it fetch the RSS-feed.
 
 =head1 SEE ALSO
 
-L<Test::Reporter>
+L<Test::Reporter>, L<testers.cpan.org>
 
 =cut
